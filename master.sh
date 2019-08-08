84fb529810b93a3362c3019f38e18dec728d3948
Thorsten Papenbrock <thorsten.papenbrock@hpi.de>
2019-08-08 11:53:39 +0200

    A few more details for the scripts.

--- master.sh
+++ master.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar target/octopus-1.0.jar master -w 0
+java -Xms60g -Xmx60g -jar target/octopus-1.0.jar master -w 0 -dn ncvoter_Statewide_100001r_71c
--- slave.sh
+++ slave.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar target/octopus-1.0.jar slave -w 20 -mh 172.16.64.61
+java -Xms60g -Xmx60g -jar target/octopus-1.0.jar slave -w 20 -mh 172.16.64.61
